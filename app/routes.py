d36fb87d5ee21f686991aadd89b284bba28effe1
simmi-hp <sparsh1009@gmail.com>
2020-10-27 20:19:14 -0400

    Template-Loops

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,22 @@ from app import flask_app
 @flask_app.route('/index')
 def index():
     user = {'username': 'User1'}
-    return render_template('index.html', user=user)
+    posts = [
+        {
+            'author': {'username': 'User2'},
+            'body': 'Wonderful day in Canada...'
+        },
+        {
+            'author': {'username': 'User3'},
+            'body': 'We the North'
+        },
+        {
+            'author': {'username': 'User4'},
+            'body': 'Eh!'
+        },
+        {
+            'author': {'username': 'User5'},
+            'body': 'Maple Leaf'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
